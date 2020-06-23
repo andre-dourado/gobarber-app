e9967accb63bbb764e1b60c5731c40865bb90501
André Dourado <andrew.dourado.freitas@gmail.com>
2020-06-23 16:43:30 -0300

    Criação do agendamento

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,7 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 import CreateAppointment from '../pages/CreateAppointment';
-import AppointementCreated from '../pages/AppointmentCreated';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createStackNavigator();
 
@@ -17,7 +17,7 @@ const AppRoutes: React.FC = () => (
   >
     <App.Screen name="Dashboard" component={Dashboard} />
     <App.Screen name="CreateAppointment" component={CreateAppointment} />
-    <App.Screen name="AppointementCreated" component={AppointementCreated} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
 
     <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
